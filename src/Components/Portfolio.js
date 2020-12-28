28b6607f15c3e5d36b8e7831d8a7b90b90e99819
Jacob Polacek <jacpolacek@gmail.com>
2020-12-28 14:32:12 -0500

    Changes to resume and currently reading sections

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -12,7 +12,7 @@ class Portfolio extends Component {
                <img alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
+                 <h5><i>{projects.title}</i> by {projects.author}</h5>
                      <p>{projects.category}</p>
                   </div>
                 </div>
@@ -30,7 +30,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Check Out Some of My Works.</h1>
+            <h1>Check out what I've been reading.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
