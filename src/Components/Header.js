69a1ba3e00c2c80fce50a74489947bca0a5be4e4
Jacob Polacek <jacpolacek@gmail.com>
2021-01-05 00:12:51 -0500

    Changing some resume data and adding gradient to nav-wrap

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -25,8 +25,9 @@ class Header extends Component {
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Books</a></li>
+	         <li><a className="smoothscroll" href="#ranks">Espresso</a></li> 
+	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
          </ul>
 
       </nav>
