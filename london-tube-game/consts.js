909cf37e9f5f9d67c605256d4e32c6be75c85e1b
Jacob Polacek <jacpolacek@gmail.com>
2024-08-03 19:45:51 -0700

    Updates

---
+++ london-tube-game/consts.js
@@ -0,0 +1,2 @@
+export const ACCESS_TOKEN = process.env.MAPBOX_GL_ACCESS_TOKEN
+export const STYLE_URI = process.env.MAPBOX_STYLE_URI
---
+++ london-tube-game/local_consts.js
@@ -0,0 +1,2 @@
+export const ACCESS_TOKEN = "pk.eyJ1IjoianBvbGFjZWsiLCJhIjoiY2xuZHp3cmI2MDhyMTJrbGZpN3N2YzZ4OSJ9.qopsbFF_yOwymy1ajYaR9w";
+export const STYLE_URI = "jpolacek/clnpkmqiw00uh01pv0mypd0i9";
