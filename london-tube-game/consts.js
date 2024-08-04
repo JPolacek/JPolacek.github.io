bcccd419c06efc371e1aafc062dda6867e11429f
JPolacek <jpolacek@gmail.com>
2024-08-04 07:11:32 +0000

    Updates

--- london-tube-game/consts.js
+++ london-tube-game/consts.js
@@ -1,2 +1,2 @@
-export const ACCESS_TOKEN = process.env.MAPBOX_GL_ACCESS_TOKEN
-export const STYLE_URI = process.env.MAPBOX_STYLE_URI
+export const ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_GL_ACCESS_TOKEN
+export const STYLE_URI = process.env.REACT_APP_MAPBOX_STYLE_URI
