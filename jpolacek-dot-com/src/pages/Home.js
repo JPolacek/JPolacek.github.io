87b6a820bbb6bb31bf1badfc346e1337231e6569
Jacob Polacek <jacpolacek@gmail.com>
2025-02-12 20:25:00 -0800

    Update resume

--- jpolacek-dot-com/src/pages/Home.js
+++ jpolacek-dot-com/src/pages/Home.js
@@ -1,25 +1,25 @@
-import './Home.css';
-import Fader from '../components/Fader'
-import ChooChoo from '../components/ChooChoo'
+import "./Home.css";
+import Fader from "../components/Fader";
+import ChooChoo from "../components/ChooChoo";
 import React from "react";
 
 function Home() {
   return (
-	<div className="App">
-	  <header className="App-header">
-		<Fader text="Jake Polacek">
-		  <p>
-		  	Here for <a className="App-link" href="JakePolacekResume2024.pdf">
-			  <span>
-			  	business?
-			  </span>
-			</a><br />
-		  	Or games? &darr;
-		  </p>
-		</Fader>
-		<ChooChoo className="choochoo" src="tube-train.png" />
-	  </header>
-	</div>
+    <div className="App">
+      <header className="App-header">
+        <Fader text="Jake Polacek">
+          <p>
+            Here for{" "}
+            <a className="App-link" href="Jake_Polacek_Resume.pdf">
+              <span>business?</span>
+            </a>
+            <br />
+            Or games? &darr;
+          </p>
+        </Fader>
+        <ChooChoo className="choochoo" src="tube-train.png" />
+      </header>
+    </div>
   );
 }
 
